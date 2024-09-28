b1cdb0bfb4dfc9a19c403e7d45212ef8507619b7
tonyisaacAog <Tony@cyrustecheg.com>
2024-09-28 20:14:11 +0300

    second Problem In StudyPlan

---
+++ LeetCode75/ArrayString/GCDSs_1071.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode75.ArrayString
+{
+
+    [Description("1071. Greatest Common Divisor of Strings")]
+    public class GCDSs_1071 : IRunFactoryMethod
+    {
+
+        #region fail trying
+        //public string GcdOfStrings(string str1,string str2)
+        //{
+
+        //    int pointerStr1 = 0;
+        //    int pointerStr2 = 0;
+        //    string sstr1 = str1.Length>str2.Length?str1:str2;
+        //    string sstr2 = str1.Length<str2.Length ? str1:str2;
+        //    StringBuilder stringBuilder = new StringBuilder();
+        //    while( pointerStr1 < sstr1.Length )
+        //    {
+        //        if( sstr2[pointerStr2] == sstr1[pointerStr1] )
+        //        {
+        //            pointerStr1++;
+        //            pointerStr2++;
+        //        }
+        //        else { return string.Empty; }
+        //        if( pointerStr2 > sstr2.Length-1 ) pointerStr2 = 0;
+        //    }
+        //    for( int i =sstr2.Length; i < sstr1.Length; i++ )
+        //    {
+        //        stringBuilder.Append(sstr1[i]);
+        //    }
+        //    return stringBuilder.ToString();
+        //}
+        #endregion
+
+
+        #region using Euclidean Algorithm (GCD of Lengths)
+        public string GcdOfStrings(string str1,string str2)
+        {
+            if( str1 + str2 != str2 + str1 )
+                return string.Empty;
+            int gcdLength = GCD(str1.Length,str2.Length);
+            return str1.Substring(0,gcdLength);
+        }
+        private int GCD(int a,int b)
+        {
+            if( b == 0 ) return a;
+            return GCD(b,a%b);
+        }
+        #endregion
+
+        #region 5. Recursive Approach (Based on GCD of Strings)
+        public string GcdOfStrings_Recursive(string str1,string str2)
+        {
+            if( str1 + str2 != str2 + str1 )
+                return string.Empty;
+            if( str2 == "" ) return str1;
+            return GcdOfStrings_Recursive(str2,str1.Substring(0,str1.Length % str2.Length));
+        }
+       
+        #endregion
+
+
+        public void Run()
+        {
+            var str = GcdOfStrings_Recursive("ABCABC","ABC");
+            Console.WriteLine(str);
+            var str2 = GcdOfStrings_Recursive("ABCDEF","ABC");
+            Console.WriteLine(str2);
+            var str3 = GcdOfStrings_Recursive("TAUXXTAUXXTAUXXTAUXXTAUXX","TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX");
+            Console.WriteLine(str3);
+        }
+    }
+}
--- LeetCode75/EntryPoint.cs
+++ LeetCode75/EntryPoint.cs
@@ -1,22 +1,88 @@
-﻿using LeetCode75.ArrayString;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace LeetCode75
 {
     public class EntryPoint
     {
-        private static IRunFactoryMethod method;
-        private static Dictionary<string,string>
-            LeetCode75Dic = new Dictionary<string,string>
-            {
-                { "1768","MergeStringsAlternately_1768" }
-            };
+        private static Dictionary<string,string> LeetCode75Dic = new Dictionary<string,string>
+        {
+            { "1768", "MergeStringsAlternately_1768" },
+            { "1071", "GCDSs_1071" }
+        };
 
-        
+        private void PrintProblems()
+        {
+            PrintOptions();
+            Console.WriteLine("####################################################################################");
+            foreach( var entry in LeetCode75Dic )
+            {
+                Console.WriteLine($"Problem : \n~ Key : {entry.Key} || Problem Name : {entry.Value}");
+            }
+            Console.WriteLine("####################################################################################");
+        }
+
+        private void PrintOptions()
+        {
+            Console.WriteLine("Options : \n~ If You Want to Exit, Enter X or x\n~ If You Want to Clear the Screen, Enter C or c");
+        }
 
         public void Start()
         {
-            IRunFactoryMethod method = new MergeStringsAlternately_1768();
-            method.Run();
+            PrintProblems();
+            while( true )
+            {
+                Console.Write("Enter Problem Key: ");
+                var input = Console.ReadLine();
+                if( input.ToLower() == "x" )
+                {
+                    Environment.Exit(0);
+                }
+                else if( input.ToLower() == "c" )
+                {
+                    Console.Clear();
+                    PrintProblems();
+                }
+                else if( LeetCode75Dic.ContainsKey(input) )
+                {
+                    var problemString = LeetCode75Dic[input];
+                    try
+                    {
+                        // Get the current assembly
+                        var assembly = Assembly.GetExecutingAssembly();
+
+                        // Fully qualified class name (namespace + class name)
+                        string ClassName = $"{problemString}";
+
+                        // Get the type (class) from the assembly
+                        var typeInfo = assembly.DefinedTypes.FirstOrDefault(obj=>obj.Name == ClassName);
+
+                        if( typeInfo != null )
+                        {
+                            // Create an instance of the type (class)
+                            var type = assembly.GetType(typeInfo.FullName);
+
+                            var instance = Activator.CreateInstance(type);
+
+                            // Invoke the "Run" method
+                            type.GetMethod("Run")?.Invoke(instance,null);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Problem not found.");
+                        }
+                    }
+                    catch( Exception ex )
+                    {
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
         }
     }
 }
